4a440917868e43a16230e9a49daf392c706dfcf1
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-07-06 14:40:27 +0700

    test a ControlInput component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h3>Please type in the box:</h3>
+        <input value={this.state.input} onChange={this.handleChange}/>
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    )
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ControlledInput />
   </React.StrictMode>
 );
 
